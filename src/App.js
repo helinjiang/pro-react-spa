a892549113d7d0c6359a145b7c8f856abad16e7b
linjianghe <linjianghe@tencent.com>
2018-06-12 15:19:48 +0800

    refactor: 引入 redux

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Topics from './pages/topics';
+import Mockers from './pages/mockers';
 
-const BasicExample = () => (
+const App = () => (
     <Router>
-        <div>
+        <div className="container">
             <ul>
                 <li>
                     <Link to="/">Home</Link>
@@ -18,6 +19,9 @@ const BasicExample = () => (
                 <li>
                     <Link to="/topics">Topics</Link>
                 </li>
+                <li>
+                    <Link to="/mockers">Mockers</Link>
+                </li>
             </ul>
 
             <hr />
@@ -25,8 +29,9 @@ const BasicExample = () => (
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/topics" component={Topics} />
+            <Route path="/mockers" component={Mockers} />
         </div>
     </Router>
 );
 
-export default BasicExample;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { connect, Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
 import App from './App';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(window.__initialState);
+
+function mapStateToProps(state) {
+    return state;
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        dispatch
+    };
+}
+
+const Root = connect(mapStateToProps, mapDispatchToProps)(App);
+
+ReactDOM.render((
+    <Provider store={store}>
+        <Root />
+    </Provider>
+), document.getElementById('root'));
 
