d87c766a26bde8452268649f33abf511f75ee8d2
linjianghe <linjianghe@tencent.com>
2018-06-12 16:15:04 +0800

    refactor: 新布局

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { Breadcrumb, Layout, Menu } from 'antd';
+
+import LayoutHeader from './components/layout-header';
 
 import Home from './pages/home';
 import About from './pages/about';
 import Topics from './pages/topics';
 import Mockers from './pages/mockers';
 
+const { Header, Content } = Layout;
+
 const App = () => (
     <Router>
-        <div className="container">
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/topics">Topics</Link>
-                </li>
-                <li>
-                    <Link to="/mockers">Mockers</Link>
-                </li>
-            </ul>
 
-            <hr />
+        <Layout className="container">
+            <Header className="header">
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['mocker']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="mocker">Mocker</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                </Menu>
+            </Header>
 
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/topics" component={Topics} />
-            <Route path="/mockers" component={Mockers} />
-        </div>
+            <LayoutHeader/>
+
+            <Layout style={{ padding: '0 24px 24px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/topics" component={Topics} />
+                    <Route path="/mockers" component={Mockers} />
+                </Content>
+            </Layout>
+
+        </Layout>
+
     </Router>
 );
 
