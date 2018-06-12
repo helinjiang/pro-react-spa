d7ab42cb93c74ca5eaaed3a144049a8adc781a45
linjianghe <linjianghe@tencent.com>
2018-06-12 13:52:16 +0800

    refactor: 重新组织代码结构

---
+++ src/pages/about/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+export default class About extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <div className="about">
+                <h2>About</h2>
+            </div>
+        );
+    }
+}
