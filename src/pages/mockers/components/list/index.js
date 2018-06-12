79b5b14dc91c2fd32b772684e4d6234bf05f7dfc
linjianghe <linjianghe@tencent.com>
2018-06-13 00:31:42 +0800

    feat: mocker 列表

--- src/pages/mockers/components/list/index.js
+++ src/pages/mockers/components/list/index.js
@@ -10,98 +10,100 @@ import ListItem from './list-item';
 import './index.less';
 
 class MockersList extends Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-        this.state = {
-            curTag: '全部'
-        };
-    }
+    this.state = {
+      curTag: '全部'
+    };
+  }
 
-    componentDidMount() {
-        this.props.loadMockerList();
-    }
+  componentDidMount() {
+    this.props.loadMockerList();
+  }
 
-    getAllTags() {
-        const { list } = this.props;
+  getAllTags() {
+    const { list } = this.props;
 
-        let arr = [];
+    let arr = [];
 
-        list.forEach((item) => {
-            arr = arr.concat(item.config.tags);
-        });
+    list.forEach((item) => {
+      arr = arr.concat(item.config.tags);
+    });
 
-        return _.uniq(arr);
-    }
+    return _.uniq(arr);
+  }
 
-    getFilterList() {
-        const { curTag } = this.state;
-        const { list } = this.props;
+  getFilterList() {
+    const { curTag } = this.state;
+    const { list } = this.props;
 
-        return list.filter(item => item.config.tags.indexOf(curTag) > -1);
-    }
+    return list.filter(item => item.config.tags.indexOf(curTag) > -1);
+  }
 
-    handleClickTag(tagName) {
-        this.setState({
-            curTag: tagName
-        });
-    }
+  handleClickTag = (tagName) => {
+    this.setState({
+      curTag: tagName
+    });
+  };
 
-    render() {
-        const { curTag } = this.state;
+  render() {
+    const { match } = this.props;
+    const { curTag } = this.state;
 
-        const tagList = this.getAllTags();
-        const filterList = this.getFilterList();
+    const tagList = this.getAllTags();
+    const filterList = this.getFilterList();
 
-        return (
-            <div className="mockers">
-                <div className="tag-wrapper">
-                    <Button.Group>
-                        {
-                            tagList.map((tagName, tagIndex) => {
-                                return <Button
-                                    key={tagIndex}
-                                    className={tagName === curTag ? 'active' : ''}
-                                    icon="tag"
-                                    onClick={this.handleClickTag.bind(this, tagName)}>{tagName}</Button>;
-                            })
-                        }
-                    </Button.Group>
-                </div>
-                <div className="list-wrapper">
-                    {
-                        filterList.map((item, index) => {
-                            return (
-                                <ListItem key={index}
-                                          index={index}
-                                          mockerItem={item}
-                                          clickTag={this.handleClickTag}
-                                />
-                            );
-                        })
-                    }
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="mockers">
+        <div className="tag-wrapper">
+          <Button.Group>
+            {
+              tagList.map((tagName, tagIndex) => {
+                return <Button
+                  key={tagIndex}
+                  className={tagName === curTag ? 'active' : ''}
+                  icon="tag"
+                  onClick={this.handleClickTag.bind(this, tagName)}>{tagName}</Button>;
+              })
+            }
+          </Button.Group>
+        </div>
+        <div className="list-wrapper">
+          {
+            filterList.map((item, index) => {
+              return (
+                <ListItem key={index}
+                          index={index}
+                          mockerItem={item}
+                          mockersPath={match.url}
+                          clickTag={this.handleClickTag}
+                />
+              );
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const { mockerListInfo } = state;
+  const { mockerListInfo } = state;
 
-    return {
-        isLoaded: mockerListInfo.isLoaded,
-        list: mockerListInfo.list
-    };
+  return {
+    isLoaded: mockerListInfo.isLoaded,
+    list: mockerListInfo.list
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        loadMockerList() {
-            return dispatch(loadMockerList());
-        }
+  return {
+    loadMockerList() {
+      return dispatch(loadMockerList());
+    }
 
-    };
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MockersList);
--- src/pages/mockers/components/list/list-item/index.js
+++ src/pages/mockers/components/list/list-item/index.js
@@ -6,43 +6,43 @@ import { NavLink } from 'react-router-dom';
 import './index.less';
 
 export default function MockerListItem(props) {
-    const { mockerItem, curTag, index, clickTag } = props;
-    const mockerItemConfig = mockerItem.config;
+  const { mockerItem, mockersPath, curTag, index, clickTag } = props;
+  const mockerItemConfig = mockerItem.config;
 
-    return (
-        <Row>
-            <Col span={24}>
-                <Card title={`${index + 1}. ${mockerItem.name}`}>
-                    <Button.Group>
-                        {
-                            mockerItemConfig.tags.map((tagName, tagIndex) => {
-                                return (
-                                    <Button
-                                        key={tagIndex}
-                                        className={tagName === curTag ? 'active' : ''}
-                                        icon="tag"
-                                        onClick={clickTag.bind(this, tagName)}>
+  return (
+    <Row>
+      <Col span={24}>
+        <Card title={`${index + 1}. ${mockerItem.name}`}>
+          <Button.Group>
+            {
+              mockerItemConfig.tags.map((tagName, tagIndex) => {
+                return (
+                  <Button
+                    key={tagIndex}
+                    className={tagName === curTag ? 'active' : ''}
+                    icon="tag"
+                    onClick={clickTag.bind(this, tagName)}>
 
-                                        {tagName}
+                    {tagName}
 
-                                    </Button>
-                                );
-                            })
-                        }
-                    </Button.Group>
+                  </Button>
+                );
+              })
+            }
+          </Button.Group>
 
-                    <div className="detail">
-                        <p>{mockerItemConfig.description}</p>
-                    </div>
+          <div className="detail">
+            <p>{mockerItemConfig.description}</p>
+          </div>
 
-                    <NavLink to={`/admin/mockers/mocker/${mockerItem.name}`}>
-                        <Button type="primary"
-                                size="large"
-                                icon="tool">更多...</Button>
-                    </NavLink>
-                </Card>
-            </Col>
-        </Row>
-    );
+          <NavLink to={`${mockersPath}/${mockerItem.name}`}>
+            <Button type="primary"
+                    size="large"
+                    icon="tool">更多...</Button>
+          </NavLink>
+        </Card>
+      </Col>
+    </Row>
+  );
 }
 
